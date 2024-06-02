e9a466490296b14e6516befc656283059d416218
Maida <maidaa.shahid@gmail.com>
2024-06-02 15:36:11 +0500

    Changed some styling of computer model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -11,7 +11,7 @@ const Computers = ({ isMobile }) => {
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-20, 90, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
@@ -21,7 +21,7 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
+        scale={isMobile ? 0.7 : 0.7}
         position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
